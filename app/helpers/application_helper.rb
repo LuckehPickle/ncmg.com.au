2ece76e50f2ec1f04d3295753e3d17c90854ab62
Sean Bailey <sean@seanbailey.io>
2018-08-23 20:41:22 +1000

    Some code base improvements, and mobile support for landing page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def nav_link(target, id, &block)
+    is_local = controller_name == 'landing'
+    link_to(is_local ? "##{target}" : root_path(anchor: target), id: id, data: { turbolinks: !is_local }, &block)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2018_07_31_055836) do
   end
 
   create_table "images", force: :cascade do |t|
-    t.string "title", default: "Untitled Image"
+    t.string "title"
     t.boolean "featured", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

4f0d07190596bb2cca383f4dbc68aa6a924ad353
Sean Bailey <sean@seanbailey.io>
2018-10-06 20:55:01 +1000

    Added codacy badge to readme and updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,11 @@
 # config valid for current version and patch releases of Capistrano
 lock '~> 3.11.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'ncmg'
+set :repo_url, 'git@github.com:sean0x42/ncmg.com.au.git'
+set :domain, 'ncmg.com.au'
+set :rvm_ruby_version, '2.5.1'
+set :rails_env, 'production'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -23,10 +26,11 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, 'config/database.yml', 'master.key'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 
+       'public/system', 'vendor/bundle', '.bundle'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -35,7 +39,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,6 +9,10 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server '206.189.73.65',
+       user: 'deploy',
+       roles: %w[app db web]
+
 # role-based syntax
 # ==================
 

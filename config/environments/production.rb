1273ca447dff7428902ebf88af0292ec5497bab3
Sean Bailey <sean@seanbailey.io>
2018-10-07 16:34:37 +1100

    Minor mail configuration change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,10 +87,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 587,
+    port: 465,
     user_name: Rails.application.credentials.dig(:email, :user_name),
     password: Rails.application.credentials.dig(:email, :password),
     authentication: 'plain',
+    ssl: true,
     enable_starttls_auto: true
   }
 

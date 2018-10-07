730265598344214e9bef90390a09842c0d1a4d49
Sean Bailey <sean@seanbailey.io>
2018-10-07 16:40:34 +1100

    Another minor email change.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,12 +87,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 465,
+    port: 587,
     user_name: Rails.application.credentials.dig(:email, :user_name),
     password: Rails.application.credentials.dig(:email, :password),
-    authentication: 'plain',
-    ssl: true,
-    enable_starttls_auto: true
+    authentication: :plain
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
